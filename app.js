b3a27f04f0c32f54a21767e7a400adea02ab37e8
SzymanskiKarol <szymanski.karol.1991@gmail.com>
2023-02-03 20:46:13 +0100

    mobile responsive

--- app.js
+++ app.js
@@ -1,10 +1,24 @@
+const popupImg = document.querySelector(".popup-image");
+
 document.querySelectorAll(".pic img").forEach(image => {
     image.onclick = () => {
-        document.querySelector(".popup-image").style.display = "block";
+        popupImg.style.display = "block";
         document.querySelector(".popup-image img").src = image.getAttribute("src");
     }
 })
 
 document.querySelector(".popup-image span").onclick = () => {
-    document.querySelector(".popup-image").style.display = "none";
+    popupImg.style.display = "none";
 }
+
+document.addEventListener("keyup", function (e) {
+    if (e.key === "Escape") {
+        popupImg.style.display = "none"
+    }
+})
+
+popupImg.addEventListener("click", function (e) {
+    if (!e.target.classList.contains("big-img")) {
+        popupImg.style.display = "none"
+    }
+})
